27833c660385dccce979dedf8a6c89d6783f0828
Dmitree <color.ivanov2016@yandex.ru>
2023-04-19 13:41:24 +0300

    init commit

---
+++ src/main/java/ru/dmitreev/telegram/bot/steptracker/SteptrackerApplication.java
@@ -0,0 +1,13 @@
+package ru.dmitreev.telegram.bot.steptracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SteptrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SteptrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/dmitreev/telegram/bot/steptracker/SteptrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.dmitreev.telegram.bot.steptracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SteptrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
